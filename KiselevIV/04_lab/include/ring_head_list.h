6cf73e9a2ed4a35a58b5e2fcc213979070322ef7
Korobokkk <bbb754748@gmail.com>
2025-04-07 21:35:00 +0300

    ring_ead_list init work with ring_head_List. delete old project01_03 lab. add test file

--- KiselevIV/04_lab/include/ring_head_list.h
+++ KiselevIV/04_lab/include/ring_head_list.h
@@ -3,4 +3,24 @@
 #define RING_HEAD_LIST_H
 #include "head_list.h"
 
+template <typename T>
+class TRingHeadList : public THeadList<T> {
+public:
+    TRingHeadList() : pStop = pHead{};
+    TRingHeadList(const TRingHeadList<T>& list);
+    ~TRingHeadList() {};
+    const TRingHeadList& operator=(const TRingHeadList<T>& s);
+    virtual void pushFront(int key, T val);
+    virtual void pushBack(int key, T val);
+    virtual void popFront();
+    virtual void popBack();
+};
+
+template <typename T>
+TRingHeadList<T>::TRingHeadList(const TRingHeadList<T>& list) : THeadList<T>(list) {
+    if (pLast != nullptr)
+        pLast->pNext = pHead;
+    pStop = pHead;
+};
+
 #endif
